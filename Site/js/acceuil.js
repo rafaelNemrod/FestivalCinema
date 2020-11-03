5a62cbab1af93c7bfcccc5acf177f57d7b492ec1
Raphael <66629802+rafaelNemrod@users.noreply.github.com>
2020-11-03 16:02:46 +0100

    Add files via upload

---
+++ Site/js/acceuil.js
@@ -0,0 +1,47 @@
+//Fonction pour le menu téléphone
+function navTel() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+
+
+//Fonction JS de SrollReveal absolument fantastique!!!!
+//Texte du bandeau
+ScrollReveal().reveal('.logoFestival', { delay: 1000, duration: 3000 });
+
+//Section votre sophrologue
+//ScrollReveal().reveal('.overlay-image', { delay: 500, duration: 1000, origin: 'bottom', distance: "50px" });
+//ScrollReveal().reveal('#votre_sophrologue', { delay: 500, duration: 1000, origin: 'top', distance: "50px" });
+
+//Citations
+//ScrollReveal().reveal('.citation', { delay: 500, duration: 1500, origin: 'bottom', distance: "50px" });
+
+//Section La sophrologie
+//ScrollReveal().reveal('#PhotoSofro', { delay: 500, duration: 1000, origin: 'bottom', distance: "50px" });
+//ScrollReveal().reveal('#La_sophrologie', { delay: 500, duration: 1000, origin: 'top', distance: "50px" });
+
+//Section Yoga du rire
+//ScrollReveal().reveal('#PhotoYoga', { delay: 500, duration: 1000, origin: 'bottom', distance: "50px" });
+//ScrollReveal().reveal('#YogaDuRire', { delay: 500, duration: 1000, origin: 'top', distance: "50px" });
+//Section Hypnose
+//ScrollReveal().reveal('#PhotoHypnose', { delay: 500, duration: 1000, origin: 'bottom', distance: "50px" });
+//ScrollReveal().reveal('#l_hypnose', { delay: 500, duration: 1000, origin: 'top', distance: "50px" });
+
+//Image de la section relaxation
+//ScrollReveal().reveal('.one', { delay: 500, duration: 2000, origin: 'bottom', distance: "50px" });
+//ScrollReveal().reveal('.two', { delay: 500, duration: 2500, origin: 'bottom', distance: "50px" });
+//ScrollReveal().reveal('.three', { delay: 500, duration: 3000, origin: 'bottom', distance: "50px" });
+//ScrollReveal().reveal('.four', { delay: 500, duration: 3500, origin: 'bottom', distance: "50px" });
+//ScrollReveal().reveal('.five', { delay: 500, duration: 4000, origin: 'bottom', distance: "50px" });
+
+
+
+
+
+ 
+  
---
+++ Site/js/programmation.js
@@ -0,0 +1,11 @@
+//Fonction pour le menu téléphone
+  function navTel() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+
+
